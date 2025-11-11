d21717502f71681b8c26940b50d443158927e2e1
burntsushi4 <maryamamr251@gmail.com>
2025-11-10 19:29:10 -0500

    Added 8-Directional Player Movement

--- Assets/Scripts/player.gd
+++ Assets/Scripts/player.gd
@@ -1,1 +1,15 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var speed: int = 400
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector:
+		velocity = input_vector * speed
+	else:
+		velocity = input_vector
+	move_and_slide()
