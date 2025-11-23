6b4a3ec602e892395de3bb96bb294ead0e928689
ElanReizas <elanreizas@gmail.com>
2025-11-23 16:14:38 -0500

    Update shop NPC and dialogue interactions
    Renamed shop NPC from 'Cat' to 'Guntro' and updated related dialogue to provide purchase options. Adjusted balloon and dialogue UI offsets for improved layout. Added item price and coin variables to object_interaction.gd and stubbed purchase logic for future item transactions.

--- Assets/Scenes/object_interaction.gd
+++ Assets/Scenes/object_interaction.gd
@@ -2,10 +2,21 @@ extends Node2D
 
 @export var dialogue_file: DialogueResource
 @export var dialogue_title: String = "start"
+@export var itemPrice: int= 10 #Dependent on item rarity
+@export var coins: int=0
 
+#on ready
+#grab item from game library, determine price of that item and populate item and armor stands
+#ensure population occurs only once and doesn't repopulate items after purchase
 func interaction():
 	if dialogue_file:
 		DialogueManager.show_dialogue_balloon(dialogue_file, dialogue_title, [self])
 
-#future method to write for inventory
-#func pickup();
+func purchase():
+	#player has attempted to purchase item via dialogue
+	#Check players gold amount
+	#If enough, check two more conditions ->
+		#If player has no stored slot for item, equip purchased item
+		#otherwise drop currently held item, equip purchased item
+	#subtract gold count
+	pass
