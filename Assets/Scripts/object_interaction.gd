abbb4c7bb2266ec1c6d184962ee42e76ade6cbfb
ElanReizas <elanreizas@gmail.com>
2025-11-23 20:37:19 -0500

    Update object_interaction.gd

--- Assets/Scripts/object_interaction.gd
+++ Assets/Scripts/object_interaction.gd
@@ -11,12 +11,3 @@ extends Node2D
 func interaction():
 	if dialogue_file:
 		DialogueManager.show_dialogue_balloon(dialogue_file, dialogue_title, [self])
-
-func purchase():
-	#player has attempted to purchase item via dialogue
-	#Check players gold amount
-	#If enough, check two more conditions ->
-		#If player has no stored slot for item, equip purchased item
-		#otherwise drop currently held item, equip purchased item
-	#subtract gold count
-	pass
