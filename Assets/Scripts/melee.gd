bcb6f99e4f6a32f6b2a38d2e3bce0d246ac81220
ElanReizas <elanreizas@gmail.com>
2025-12-01 23:00:33 -0500

    Melee working as intended

--- Assets/Scripts/melee.gd
+++ Assets/Scripts/melee.gd
@@ -3,8 +3,14 @@ extends State
 
 func enter():
 	super.enter()
+	
+	# STOP ALL MOVEMENT
+	owner.set_physics_process(false)
+	owner.velocity = Vector2.ZERO
 	animation_player.play("SlashAttack")
+
 func transition():
 	#if too far, stop melee go back to follow
-	if owner.direction.length() > 30:
+	if owner.direction.length() > 55:
+		animation_player.play("RESET")
 		get_parent().change_state("Follow")
