34891b68c2d0c38187504687e512d5e2c8959bb5
ElanReizas <elanreizas@gmail.com>
2025-12-03 11:32:52 -0500

    Update follow.gd

--- Assets/Scripts/follow.gd
+++ Assets/Scripts/follow.gd
@@ -15,7 +15,7 @@ func transition():
 		#if target is close enough do this
 		get_parent().change_state("Melee")
 		#if player is too far dash to them
-	elif distance > 350:
+	elif distance > 270:
 		get_parent().change_state("LaserBeam")
 	elif distance > 450:
 		get_parent().change_state("Dash")
