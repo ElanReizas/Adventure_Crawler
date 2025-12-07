e7d1498e4616ff88eda73377f9fde99905fb8bc2
ElanReizas <elanreizas@gmail.com>
2025-12-07 18:34:14 -0500

    Refactor bomb scene and add explosion sprite
    Updated Bomb.tscn to use a dedicated bomb.gd script and added an explosion sprite using Blast.png. Refactored bomb.gd to handle movement, collision, and damage logic. Cleaned up Bullet.tscn by removing unused animations and assets. Added Blast.png asset for explosion visuals.

--- Assets/Scenes/bomb.gd
+++ Assets/Scenes/bomb.gd
@@ -1,11 +1,30 @@
 extends Area2D
+class_name Bomb
 
+@export var speed: float = 600.0
+@export var weapon: Weapon
+var direction: Vector2 = Vector2.ZERO
+var attacker: Node = null
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.body_entered.connect(_on_body_entered)
 
+func _physics_process(delta: float) -> void:
+	if direction != Vector2.ZERO:
+		position += direction * speed * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body):
+	if body == attacker:
+		return
+	
+	#this is an ad hoc fix for now. when the enemy dies, 
+	#the projectile still exists but its stored references (ie, attacker) become invalid
+	#as it points to freed nodes
+	if !is_instance_valid(attacker) or !is_instance_valid(weapon):
+		queue_free()
+		return
+		
+	if body in weapon.get_targets(attacker):
+		var final_damage = weapon.calculate_damage(attacker)
+		body.take_damage(final_damage)
+	queue_free()
