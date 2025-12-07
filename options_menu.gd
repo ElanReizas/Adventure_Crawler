cd326846679c6f5ffa9777b6dddd1a27b4b2845a
Rayhona Nasimova <rayhana.nasimova@temple.edu>
2025-12-07 17:48:07 -0500

    Added images

--- options_menu.gd
+++ options_menu.gd
@@ -15,3 +15,5 @@ func _on_button_pressed() -> void:
 	AudioServer.set_bus_volume_db(0, linear_to_db($AudioOptions/VBoxContainer/MasterSlider.value))
 	AudioServer.set_bus_volume_db(1, linear_to_db($AudioOptions/VBoxContainer/SFXSlider.value))
 	AudioServer.set_bus_volume_db(2, linear_to_db($AudioOptions/VBoxContainer/MusicSlider.value))
+	
+	get_tree().change_scene_to_file("res://Assets/Scenes/options_menu.tscn")
