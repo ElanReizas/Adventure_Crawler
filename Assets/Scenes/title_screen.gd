88f4ec429be966f4e3980dfb3ffe780bf816eea5
Rayhona Nasimova <rayhana.nasimova@temple.edu>
2025-12-08 10:46:27 -0500

    Main Menu changes

--- Assets/Scenes/title_screen.gd
+++ Assets/Scenes/title_screen.gd
@@ -1,5 +1,9 @@
 extends Control
 
+var selected_weapon: String = "melee"
+var selected_mode: String = "single"
+
+
 func _ready() -> void:
 	pass
 
@@ -10,7 +14,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Assets/Scenes/pursuitVsWall.tscn")# Replace with function body.
+	get_tree().change_scene_to_file("res://Assets/Scenes/Floor 1/1_Beginning_Borough.tscn")# Replace with function body.
 
 
 func _on_weapon_type_pressed() -> void:
@@ -31,3 +35,14 @@ func _on_load_game_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_option_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	var selected_text = $"VBoxContainer/WeaponType/OptionButton".get_item_text(index)
+	print("Selected:", selected_text)
+	if selected_text == "melee":
+		GameManager.player.weapon_type = GameManager.player.weapon_type.MELEE
